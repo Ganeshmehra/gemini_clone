701ed2c5623167eaffea7e8206e10de7f3c8ca29
ganesh <ganeshmehra776@gmail.com>
2024-12-24 13:59:53 +0530

    AI clone

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import Mainbox from './Mainbox'
+
+const App = () => {
+  return (
+   <>
+   <Sidebar/>
+   <Mainbox/>
+   </>
+  )
+}
+export default App
